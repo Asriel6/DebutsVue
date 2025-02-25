901476144784bbf736d0851798044eee8ebb777e
Ragot <alexis.ragot@etu.unicaen.fr>
2025-02-25 09:55:52 +0100

    Fin TD2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const text = ref("Tonk");
+const trimmedtext = computed(() => text.value.trim());
 const posts = ref([]);
 
 function addPost() {
-  posts.value.push(text.value);
+  const newPost = {
+    id: Math.random().toString(36).substring(2),
+    content: trimmedtext.value,
+    createdAt: new Date(),
+    author: {
+      id: Math.random().toString(36).substring(2),
+      username: "Asriel",
+      avatarUrl: "https://i.pinimg.com/736x/fd/7a/31/fd7a317b61c79bbe72935d1920b0698c.jpg",
+    },
+  };
+  posts.value.push(newPost);
   text.value = "";
 }
 </script>
@@ -15,11 +26,31 @@ function addPost() {
     <div class="container">
       <form class="card" @submit.prevent="addPost">
         <!--v-model lie la variable text avec l'input et l'actualise en direct-->
-        <textarea name="post" id="post" placeholder="Quoi de neuf ?" v-model="text"></textarea>
-        <button type="submit">Publier</button>
+        <textarea
+          name="post"
+          id="post"
+          placeholder="Quoi de neuf ?"
+          maxlength="200"
+          v-model="text"
+        ></textarea>
+        <button type="submit" :disabled="!trimmedtext.length">Publier</button>
       </form>
 
-      <p v-for="(post ,index) in posts" :key="index">{{ post }}</p>
+      <p v-if="!posts.length">Pas de posts pour le moment...</p>
+
+      <article v-for="(post, index) in posts" :key="index" class="card">
+        <div class="post-header">
+          <img
+            :src="post.author.avatarUrl"
+            alt="avatar"
+            width="36"
+            height="36"
+            class="user-image"
+          />
+          <a>{{ post.author.username }}</a>
+        </div>
+        <p>{{ post.content }}</p>
+      </article>
     </div>
   </main>
 </template>
@@ -65,5 +96,25 @@ button {
   font-size: 1rem;
   height: 40px;
   padding: 0 1rem;
+}
+article {
+  padding: 0.75rem 1.5rem;
+  overflow: hidden;
+}
+button:disabled {
+  cursor: not-allowed;
+  opacity: 0.4;
+}
+article p {
+  white-space: pre-warp;
+}
+.user-image {
+  border-radius: 50%;
+  object-fit: cover;
+}
+.post-header {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
 }
 </style>
